7ac92fbb4eef24c33a6a574b23a028c7a463a790
HRISHIT05 <hrishitpandey_pe22b14_15@dtu.ac.in>
2024-08-07 12:13:45 +0530

    updating

---
+++ 1. Introduction to C++/UnaryOperator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "UNARY OPERATORS" << endl;
+    int a = 5;
+    cout << a << endl;
+    cout << (++a) << endl;
+    cout << (a++) << endl;
+    cout << a << endl;
+    cout << (--a) << endl;
+    cout << (a--) << endl;
+    cout << a << endl;
+}
