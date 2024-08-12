fdbf1e15b963b33899b3358fbac85cb6ead362e5
HRISHIT05 <hrishitpandey_pe22b14_15@dtu.ac.in>
2024-08-12 22:23:49 +0530

    updating

---
+++ 4.Week-2 Assignments/NumericHollowInvertedHalfPyramid.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+// numeric hollow inverted half pyramid
+
+int main()
+{
+    int n;
+    cin>>n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j <= n; j++)
+        {
+            if (i == 0 || j == i + 1 || j == n)
+            {
+                cout << j ;
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
