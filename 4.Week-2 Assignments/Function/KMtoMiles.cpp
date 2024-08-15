88c04c3f3d0d6d661cbcc1bdd4f457cdca0724c9
HRISHIT05 <hrishitpandey_pe22b14_15@dtu.ac.in>
2024-08-15 12:36:32 +0530

    updating

--- 4.Week-2 Assignments/Function/KMtoMiles.cpp
+++ 4.Week-2 Assignments/Function/KMtoMiles.cpp
@@ -1,16 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-//KM to Miles
-float KMtoMiles(float km){
-    float _1kmMile= 0.621371;
+// KM to Miles
+float KMtoMiles(float km)
+{
+    float _1kmMile = 0.621371;
     float miles = km * _1kmMile;
     return miles;
 }
-int main(){
+int main()
+{
     float km;
-    cout<<"enter value in km: ";
-    cin>>km;
-    float miles=KMtoMiles(km);
-    cout<<"value in miles: "<<miles<<endl;
+    cout << "enter value in km: ";
+    cin >> km;
+    float miles = KMtoMiles(km);
+    cout << "value in miles: " << miles << endl;
     return 0;
 }
--- 4.Week-2 Assignments/Function/checkPrime.cpp
+++ 4.Week-2 Assignments/Function/checkPrime.cpp
@@ -1,24 +1,30 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-//function to check whether given number is prime or not
-bool checkPrime(int n){
-    int i=2;
-    for(i=2;i<n;i++){
-        if((n%i)==0){
+// function to check whether given number is prime or not
+bool checkPrime(int n)
+{
+    int i = 2;
+    for (i = 2; i < n; i++)
+    {
+        if ((n % i) == 0)
+        {
             return false;
         }
     }
     return true;
 }
 
-int main(){
-    int n=5;
+int main()
+{
+    int n = 5;
     bool isPrime = checkPrime(n);
-    if(isPrime){
-        cout<<"prime\n";
+    if (isPrime)
+    {
+        cout << "prime\n";
     }
-    else{
-        cout<<"not prime\n";
+    else
+    {
+        cout << "not prime\n";
     }
     return 0;
 }
